ba798916f679d1c0d5f60ac53e19d4b68837f682
Juan Santa <jcsantahurtado@gmail.com>
2022-04-26 23:46:50 -0500

    Creating a Modal Box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
-const modal = document.querySelector(".modal");
-const trigger = document.querySelector(".trigger");
-const closeButton = document.querySelector(".close-button");
 
-function toggleModal() {
-    modal.classList.toggle("show-modal");
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+var enlaces = document.querySelectorAll('#myBtn');
+console.log(enlaces);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function () {
+//     modal.style.display = "block";
+// }
+
+enlaces.forEach(enlace => {
+    enlace.onclick = function () {
+        console.log(`Hi`);
+        modal.style.display = 'block';
+    }
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
-function windowOnClick(event) {
-    if (event.target === modal) {
-        toggleModal();
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
-
-trigger.addEventListener("click", toggleModal);
-closeButton.addEventListener("click", toggleModal);
-window.addEventListener("click", windowOnClick);
